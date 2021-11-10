6d5c838afb334ba0dc2e5376aa48bac9bd23bed0
Rajiv.Kurien <rajiv.kurien@zuhlke.com>
2021-11-10 11:38:34 +0000

    Shuffled list

--- src/main/kotlin/TableMapper.kt
+++ src/main/kotlin/TableMapper.kt
@@ -1,21 +1,21 @@
 class TableMapper {
 
-    val csvToGroups = ReadCSVToGroups()
+    private val csvToGroups = ReadCSVToGroups()
 
     fun getSeatingPlanFor(
         noOfTables: Int,
         tableCapacity: Int,
         fileName: String
     ): List<Pair<String, List<String>>> {
-        val groups = csvToGroups.readToGroups(fileName)
-        val tables = emptyTables(noOfTables, tableCapacity)
-        val distribution = distributeGroups(tables, groups.mapValues { it.value.size })
-        return assignPeople(distribution, groups)
+        val groups: Map<Group, List<String>> = csvToGroups.readToGroups(fileName)
+        val distribution: List<Table> =
+            distributeGroups(emptyTables(noOfTables, tableCapacity), groups.mapValues { it.value.size })
+        return assignPeople(distribution, groups.mapValues { it.value.shuffled() })
     }
 
-    internal fun emptyTableWithCapacity(capacity: Int) = Table(capacity, mutableMapOf())
+    private fun emptyTableWithCapacity(capacity: Int) = Table(capacity, mutableMapOf())
 
-    internal fun emptyTables(noOfTables: Int, tableCapacity: Int): List<Table> {
+    private fun emptyTables(noOfTables: Int, tableCapacity: Int): List<Table> {
         val tables = mutableListOf<Table>()
         repeat(noOfTables) {
             tables.add(emptyTableWithCapacity(tableCapacity))
--- src/test/kotlin/TableMapperTest.kt
+++ src/test/kotlin/TableMapperTest.kt
@@ -1,20 +1,41 @@
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class TableMapperTest {
 
-    val unit = TableMapper()
+    private val unit = TableMapper()
 
     @Test
     fun `seating plan for Sample1`() {
         val plan = unit.getSeatingPlanFor(noOfTables = 3, tableCapacity = 2, fileName = "Sample1.csv")
 
-        assertEquals(
-            listOf(
-                "Table0" to listOf("Alpha", "Charlie"),
-                "Table1" to listOf("Bravo", "Echo"),
-                "Table2" to listOf("Delta"),
-            ), plan
-        )
+        with(plan[0].second) {
+            assertTrue(
+                this.size == 2
+                        && (this.contains("Charlie") || this.contains("Delta"))
+                        && (this.contains("Alpha") || this.contains("Bravo"))
+            )
+        }
+        with(plan[1].second) {
+            assertTrue(
+                this.size == 2
+                        && this.contains("Echo")
+                        && (this.contains("Alpha") || this.contains("Bravo"))
+            )
+        }
+        with(plan[2].second) {
+            assertTrue(
+                this.size == 1
+                        && (this.contains("Charlie") || this.contains("Delta"))
+            )
+        }
+
+        assertTrue(plan[2].second.size == 1)
+
+//            listOf(
+//                "Table0" to listOf("Alpha" | "Bravo", "Charlie" | "Delta"),
+//                "Table1" to listOf("Alpha" | "Bravo", "Echo"),
+//                "Table2" to listOf("Charlie" | "Delta"),
+//            )
     }
 }
